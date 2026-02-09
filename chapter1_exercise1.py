69568284bdf2a7d5b50c525c22a70e143b3665d4
Silicon Valley QA <165145438+siliconvalleyqa@users.noreply.github.com>
2026-02-09 00:03:31 -0800

    Add exercise 1 of python

---
+++ chapter1_exercise1.py
@@ -0,0 +1,19 @@
+'''
+CIS 117 - Chapter 1 Exercise 1 - Erick Ramos
+
+This is my first program in Python!
+'''
+
+age = 1
+print("Age=", age)
+
+age = age + 1
+print("Age=", age)
+print("Done!")
+
+'''
+Output:
+Age= 1
+Age= 2
+Done!
+'''
